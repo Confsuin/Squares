4bdad346117a9232a3df5c994d80a0261f751072
Confsuin <Lukas.bottiger@icloud.com>
2025-01-21 14:33:01 +0100

    Start Of Cards

---
+++ Assets/Card.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    // Intergers. Numbers in Decimal form
+    public int Health = 1;
+    public int Damage = 1;
+    
+    public int BulletSpeed = 1;
+    public int BulletSize = 1;
+    public int BulletSlow = 1;
+
+    public int ReloadSpeed = 1;
+    public int AttackSpeed = 1;
+
+    public int Range = 1;
+    
+    // Intergers. Numbers in flat amount form
+    public int BulletPoison;
+    public int BulletBounces;
+    public int BulletCount;
+
+    public int Ammo;
+    
+    public int BlockCoolDown;
+    public int BlockCount;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/CardHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardHandler : MonoBehaviour
+{
+    // List of Cards
+    public static List<object> Cards = new List<object>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
