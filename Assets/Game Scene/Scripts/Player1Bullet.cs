b7538e89566bd31dcd0e4eb9bc8a23c638c61d8d
Confsuin <Lukas.bottiger@icloud.com>
2025-04-29 14:16:25 +0200

    Bouncy Bullet FULLY working(Pray)

--- Assets/Game Scene/Scripts/Player1Bullet.cs
+++ Assets/Game Scene/Scripts/Player1Bullet.cs
@@ -14,6 +14,10 @@ public class Player1Bullet : MonoBehaviour
     public float fire;
     public float explosion;
 
+    public bool isBounced = false;
+    public float TimesBouncedTimer = 0f;
+    public float BouncedTimerSpeed = 0.005f;
+
     public GameObject explosionEffect;
     public GameObject fireZoneEffect;
 
@@ -41,7 +45,18 @@ public class Player1Bullet : MonoBehaviour
 
         rb2d.AddForce(transform.right * speed);
     }
+    private void FixedUpdate()
+    {
+        if (isBounced == true)
+        {
+            TimesBouncedTimer -= Time.deltaTime;
 
+            if (TimesBouncedTimer >= BouncedTimerSpeed)
+            {
+                isBounced = false;
+            }
+        }
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -77,7 +92,12 @@ public class Player1Bullet : MonoBehaviour
         }
         if (other.gameObject.tag == "Wall")
         {
-            timesBounced += 0.5f;
+            if (TimesBouncedTimer <= 0)
+            {
+                isBounced = true;
+                TimesBouncedTimer = BouncedTimerSpeed;
+                timesBounced += 1f;
+            }
         }
         if (other.gameObject.tag == "Wall" && timesBounced >= bounce)
         {
