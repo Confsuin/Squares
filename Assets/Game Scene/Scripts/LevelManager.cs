ac49a14dda2ce5a42461fdcb580b8f6e21837928
Confsuin <Lukas.bottiger@icloud.com>
2025-05-24 20:55:32 +0200

    Update LevelManager.cs

--- Assets/Game Scene/Scripts/LevelManager.cs
+++ Assets/Game Scene/Scripts/LevelManager.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class LevelManager : MonoBehaviour
 {
