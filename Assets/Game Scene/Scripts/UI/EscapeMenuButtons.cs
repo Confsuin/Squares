3896c2f91a15fcb5691203934a8ded2d006ea30a
Confsuin <Lukas.bottiger@icloud.com>
2025-05-24 20:54:59 +0200

    s

--- Assets/Game Scene/Scripts/UI/EscapeMenuButtons.cs
+++ Assets/Game Scene/Scripts/UI/EscapeMenuButtons.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class EscapeMenuButtons : MonoBehaviour
 {
